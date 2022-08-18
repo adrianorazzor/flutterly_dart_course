d9451d61de8ef5d5926b4be4a11214c0fbc4501b
Adriano <adrianorazzor@gmail.com>
2022-08-18 19:53:45 -0400

    Initial commit

---
+++ bin/flutterly_dart_course.dart
@@ -0,0 +1,5 @@
+import 'package:flutterly_dart_course/flutterly_dart_course.dart' as flutterly_dart_course;
+
+void main(List<String> arguments) {
+  print('Hello world: ${flutterly_dart_course.calculate()}!');
+}
---
+++ test/flutterly_dart_course_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutterly_dart_course/flutterly_dart_course.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
